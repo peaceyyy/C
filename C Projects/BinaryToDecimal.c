2061ce4980e1cd65f481931c64c48c1971be85be
Homer Adriel Dorin <154606101+peaceyyy@users.noreply.github.com>
2025-03-23 19:56:38 +0800

    Create BinaryToDecimal.c

---
+++ C Projects/BinaryToDecimal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int *convertBinaryToDeci(int binaryNum, int bitStringLength)
+{
+    if (binaryNum == 0) return 0;
+    
+    int decimalNum = 0;
+    
+    while (binaryNum > 0)
+    {
+        int binaryDigit = binaryNum % 10;
+        
+        if (binaryDigit == 1)
+        {
+            decimalNum += pow(2, bitStringLength);
+        }
+        
+        binaryNum /= 10;
+        bitStringLength--;
+        
+    }
+    return decimalNum;
+}
+
+int main()
+{
+    printf("%d", convertBinaryToDeci(1011, 4));
+    
+
+    return 0;
+}
